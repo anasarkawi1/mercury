Fixed display and usage issues, Added basic calculation functions (percentage change, moving average), Added execution of basic calculations for historic data (no live update).

class_data.py
@@ -6,6 +6,7 @@
 # Import libraries
 from binance.spot import Spot
 from binance.websocket.spot.websocket_stream import SpotWebsocketStreamClient as WebSocketClient
+import pandas as pd
 from pandas import DataFrame, concat
 import numpy as np
 import json
@@ -15,12 +16,33 @@ import json
 import signal
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 
+# Sets pandas disable option
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+
 
 # TODO: Fix function return values
 
 
 # Data
 class DataReq:
+    # Indicators calculation functions
+    # TODO: Seperate these functions into "class_calculation.py" file
+    # Calculate percentage change
+    def pChangeFunc(self, old, new):
+        pChange = new - old
+        pChange = pChange / old
+        pChange = pChange * 100
+        return pChange
+    
+    # Moving average
+    # TODO: A system has to be implemented for moving averages specifically due to their high flexibility. A similar system should be implemented for EMAs they're being implemented.
+    def movingAvg(self, prices):
+        average = 0
+        for price in prices:
+            average += price['close']
+        average = average / len(prices)
+        return average
+
     # Request kline data
     # Data format: [open time, open price, high price, low price, close price, volume, close time, quote asset volume, number of trades, taker buy base asset volume, taker buy quote asset volume, unused]
     def requestKline(self):
@@ -29,6 +51,7 @@ class DataReq:
 
         columns = self.dataColumns
         for price in self.rawData:
+            # Insert data into DataFrame
             columns['openTime'].append(float(price[0]))
             columns['open'].append(float(price[1]))
             columns['high'].append(float(price[2]))
@@ -36,7 +59,11 @@ class DataReq:
             columns['close'].append(float(price[4]))
             columns['closeTime'].append(float(price[6]))
             columns['volume'].append(float(price[5]))
-            columns['pChange'].append(np.nan)
+
+        # Insert NaN for data yet to be calculated
+        columns['pChange'] = np.nan
+        columns['rsi'] = np.nan
+        columns['ma5'] = np.nan
 
         self.data = DataFrame(data=columns)
 
@@ -44,18 +71,26 @@ class DataReq:
         # TODO: Uses manual iteration, optimize for faster results. (pandas.DataFrame.apply?)
         pVals = [np.nan]
         for i in range(0, len(self.data) - 1):
-            pChange = self.data['close'][i + 1] - self.data['close'][i]
-            pChange = pChange / self.data['close'][i]
-            pChange = pChange * 100
-            pVals.append(pChange)
+            pVals.append(self.pChangeFunc(self.data['close'][i + 1], self.data['close'][i]))
         self.data['pChange'] = pVals
-        self.data = self.data.dropna()
 
-    def pChangeFunc(self, old, new):
-        pChange = new - old
-        pChange = pChange / old
-        pChange = pChange * 100
-        return pChange
+        # Calculate moving average (length = 5)
+        avgArr = []
+        maLen = 5
+        for i in range(0, len(self.data) - 1):
+            if len(avgArr) != maLen:
+                avgArr.append(self.data['close'][i])
+            else:
+                avg = np.sum(avgArr)
+                avg = avg / maLen
+                # self.data.loc[price['openTime']]['ma5'] = avg
+                self.data['ma5'][i] = avg
+                avgArr.append(self.data['close'][i])
+                avgArr.pop(0)
+
+
+        # Drop NaN should only be used when all initial calculations have been done.
+        # self.data = self.data.dropna()
 
     # Live price updater.
     def wsHandler(self, _, msg):
@@ -70,11 +105,24 @@ class DataReq:
         kline = processed['k']
 
         # Convert dict into suitable array
-        # [openTime, open, high, low, close, closeTime, volume, pChange.]
-        klineArr = [int(kline['t']), float(kline['o']), float(kline['h']), float(kline['l']), float(kline['c']), int(kline['T']), float(kline['v']), self.pChangeFunc(self.data['close'][len(self.data) - 1], float(kline['c']))]
+        # [openTime, open, high, low, close, closeTime, volume, pChange.}
+        # TODO: Standardise declerations.
+        klineArr = [
+            int(kline['t']),
+            float(kline['o']),
+            float(kline['h']),
+            float(kline['l']),
+            float(kline['c']),
+            int(kline['T']),
+            float(kline['v']),
+            self.pChangeFunc(self.data['close'][len(self.data) - 1],
+            float(kline['c'])),
+            np.nan,
+            np.nan
+            ]
 
         # Update new price
-        self.data.loc[len(self.data)] = klineArr
+        self.data.loc[len(self.data) - 1] = klineArr
 
         print(self.data)
 
@@ -90,14 +138,19 @@ class DataReq:
 
     # Instance initialization
     def __init__(self, tradingPair, interval, limit):
+        # Define options for the trader instance
         self.options = {
             'tradingPair': tradingPair,
             'interval': interval,
             'limit': limit,
             'threshold': 0.25
         }
+        
+        # Define Binance clients for the REST API and WebSocket interfaces.
         self.client = Spot()
         self.wsClient = WebSocketClient(on_message=self.wsHandler)
+
+        # Define column names for the pandas DataFrame
         self.dataColumns = {
             'openTime': [],
             'open': [],
@@ -106,8 +159,12 @@ class DataReq:
             'close': [],
             'closeTime': [],
             'volume': [],
-            'pChange': []
+            'pChange': [],
+            'rsi': []
         }
+
+        # Price levels
+        # TODO: Implement level and price action calculations
         self.levels = {
             'resistance': [],
             'support': DataFrame(data=self.dataColumns)