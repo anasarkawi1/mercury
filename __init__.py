16247131c40a8be6a0148bd081548a475e16b605
Anas Arkawi <anasarkawicontact@gmail.com>
2024-07-10 01:19:12 +0300

    Fixed imports.

--- __init__.py
+++
@@ -1,4 +0,0 @@
-# Mercury Library
-# By Anas Arkawi, 2024.
-from mercury import connector
-from mercury import class_calculation
---
+++ mercuryFramework/__init__.py
@@ -0,0 +1,4 @@
+# Mercury Library
+# By Anas Arkawi, 2024.
+from . import connector
+from . import class_calculation
